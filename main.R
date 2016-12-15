42da93c50c321a3e7dd3bc5f56a552c84751d6ea
mbenigni <matthew.benigni@gmail.com>
2016-12-15 18:21:02 -0500

    final changes

--- main.R
+++ main.R
@@ -1,30 +1,74 @@
 rm(list=ls())
-source('/Users/mbenigni/Public/repositories/IVCC/IVCC_Features.R')
-source('simFunctions.R')
-load('CJTC_M.Rdata')
+source('~/Public/repositories/IVCC/IVCC_Features.R')
+source('simFunctions_working.R')
+load('CJTC_M.RData')
 
-alpha_seq=seq(0,1,by=.025)
+M=graph.empty(n=length(a$userID),directed=FALSE)
+V(M)$name=as.character(a$userID)
+M=add_edges(M,edges=rbind(as.character(m[,1]),as.character(m[,2])))
+membershipM=as.vector(membership(cluster_louvain(M)))
+
+cDF=as.data.frame(ftable(membershipM))
+groupsM=sum(cDF$Freq>1)
+
+alpha_seq=seq(.025,.5,by=.025)
+replicates=2
+dens=trans=dia=dyad=triad=groups=users=as.data.frame(array(NA,dim=c(length(alpha_seq),replicates)),
+                                        row.names=as.character(alpha_seq))
+
+min.clust.par=100
+for(i in 1:length(alpha_seq)){
+  for(j in 1:replicates){
+    obj=simIteration(nodeList=a$userID,g=m,min.clust=min.clust.par,alpha=alpha_seq[i],replace=FALSE,add=TRUE,membershipM)
+    dens[i,j]=obj$dens
+    trans[i,j]=obj$trans
+    dia[i,j]=obj$dia
+    dyad[i,j]=obj$dyad
+    triad[i,j]=obj$triad
+    users[i,j]=obj$users
+    groups[i,j]=obj$groups
+  }
+}
+
+save(alpha_seq,min.clust.par,dens,trans,dia,dyad,triad,file='VirtualExperiment2_add.RData')
+
+alpha_seq=seq(.025,.5,by=.025)
+replicates=100
+dens=trans=dia=dyad=triad=as.data.frame(array(NA,dim=c(length(alpha_seq),replicates)),
+                                        row.names=as.character(alpha_seq))
+
+for(i in 1:length(alpha_seq)){
+  for(j in 1:replicates){
+    obj=simIteration(nodeList=a$userID,g=m,min.clust=min.clust.par,alpha=alpha_seq[i],replace=FALSE,add=FALSE)
+    dens[i,j]=obj$dens
+    trans[i,j]=obj$trans
+    dia[i,j]=obj$dia
+    dyad[i,j]=obj$dyad
+    triad[i,j]=obj$triad
+    users[i,j]=obj$users
+    groups[i,j]=obj$groups
+  }
+}
+
+save(alpha_seq,min.clust.par,dens,trans,dia,dyad,triad,file='VirtualExperiment2_subtract.RData')
+
+
+alpha_seq=seq(.025,1,by=.025)
 replicates=100
 dens=trans=dia=dyad=triad=as.data.frame(array(NA,dim=c(length(alpha_seq),replicates)),
                                         row.names=as.character(alpha_seq))
 
 min.clust.par=20
-                                        
-
-s=proc.time()
-obj=simIteration(nodeList=a$userID,g=m,min.clust=min.clust.par,alpha=.25,replace=TRUE,add=TRUE)
-d=(proc.time()-s)
-
-d*replicates*length(alpha_seq)/3600
-
-for(i in 1:length(alpha)){
+for(i in 1:length(alpha_seq)){
   for(j in 1:replicates){
     obj=simIteration(nodeList=a$userID,g=m,min.clust=min.clust.par,alpha=alpha_seq[i],replace=TRUE,add=TRUE)
-    dens[i,j]=obj$dense
+    dens[i,j]=obj$dens
     trans[i,j]=obj$trans
     dia[i,j]=obj$dia
     dyad[i,j]=obj$dyad
     triad[i,j]=obj$triad
+    users[i,j]=obj$users
+    groups[i,j]=obj$groups
   }
 }
 
--- simFunctions.R
+++ simFunctions.R
@@ -1,70 +1,103 @@
 # What if we build an iterative model? One that continues to sub-group louvain groups until we meet 
 # some minimal group size threshold.  Synthetic Nested Louvain Block Models.
 # keep M as an edge list, just continue to work the diagonal in nested fashion
-
-
-get_clusters=function(nodeList,g,min.clust=5,alpha,replace,add){
-  
-  getEdges=function(nodesA,nodesB,g,alpha,replace=TRUE,add=TRUE){
-    original.edges=g[(g[,1] %in% nodesA & g[,2] %in% nodesB)|(g[,1] %in% nodesB & g[,2] %in% nodesA),]
-    ecount=dim(original.edges)[1]
-    n=ceiling(alpha*ecount)
-    random.edges=edges=as.data.frame(cbind(sample(nodesA,n,replace=TRUE),sample(nodesB,n,replace=TRUE)))
-    names(original.edges)=names(random.edges)=c('Source','Target')
-    if(replace){
-      edges=rbind(original.edges[-1*sample(1:ecount,n),],random.edges)
-    }else{
-      if(add){
-        edges=rbind(original.edges,random.edges)
-      }else{
-        edges=original.edges[sample(1:ecount,ecount-n),]
-      }
-    }
-    return(edges)
-  }
-  g=g[(g$Source %in% nodeList & g$Target %in% nodeList),]
-  G=graph.edgelist(cbind(as.character(g$Source),as.character(g$Target)),directed=FALSE)
-  nDF=data.frame(node=V(G)$name,cluster=as.vector(membership(cluster_louvain(G))),stringsAsFactors=FALSE)
-  cDF=as.data.frame(ftable(nDF$cluster))
+clusterList=function(vector,min.clust){
+  cDF=as.data.frame(ftable(vector))
   names(cDF)=c('cluster','count')
-  cDF$cluster=as.numeric(as.character(cDF$cluster))
-  newClust=max(cDF$cluster)+1
-  smallClusters=cDF$cluster[cDF$count<min.clust]
-  smallNodes=nDF$node[nDF$cluster %in% smallClusters]
-  nDF=nDF[!(nDF$node %in% smallNodes),]
-  clist=unique(nDF$cluster)
-  
-  if(length(clist)>1){
-    clustList=lapply(unique(nDF$cluster),function(x) nDF$node[nDF$cluster==x])}else{
-    clustList=list()
-    clustList[[1]]=nDF$node
-    }
-  if(length(clustList)==0){
-    clustList=NULL
-    edges=getEdges(nodesA=nodeList,nodesB=nodeList,g=g,alpha,replace,add)
+  if(any(cDF$count>=min.clust)){
+    cDF=cDF[order(cDF$count,decreasing=TRUE),]
+    cDF$cluster=as.numeric(as.character(cDF$cluster))
+    smallClusters=cDF$cluster[cDF$count<min.clust]
+    cDF=cDF[cDF$count>=min.clust,]
+    return(list(cDF=cDF,smallClusters=smallClusters)) 
   }else{
-   
-  clustList[[length(clustList)+1]]=smallNodes
+    return(list(cDF=NULL,smallClusters=cDF$cluster))
+  }
   
-  edges=getEdges(nodesA=clustList[[length(clustList)]],nodesB=clustList[[length(clustList)]],g=g,alpha,replace,add)
-  cdim=length(clustList)
-  for(i in 1:(cdim-1)){
-    for(j in (i+1):cdim){
-      edges=rbind(edges,
-                  getEdges(nodesA=clustList[[i]],
-                           nodesB=clustList[[j]],g=g,alpha,replace,add))
-    }
-  }
-  clustList[[cdim]]=NULL
-  }
-  obj=list(clustList=clustList,edgeList=edges)
-  return(obj)
 }
 
-simIteration=function(nodeList,g,min.clust,alpha,replace,add){
-  clustOBJ=get_clusters(nodeList,g,min.clust,alpha,replace,add)
-  queue=clustOBJ$clustList
-  edges=clustOBJ$edgeList
+getEdges=function(nodesA,nodesB,g,alpha,replace=TRUE,add=TRUE){
+  indecies=(g$Source %in% nodesA & g$Target %in% nodesB)|(g$Source %in% nodesB & g$Target %in% nodesA)
+  edges=g[indecies,]
+  ecount=sum(indecies)
+  n=ceiling(alpha*ecount)
+  random.edges=as.data.frame(cbind(sample(nodesA,n,replace=TRUE),sample(nodesB,n,replace=TRUE)),stringsAsFactors=FALSE)
+  
+  if(replace){
+    edges[sample(1:ecount,n),]=random.edges
+  }else{
+    if(add){names(random.edges)=c('Source','Target')
+            edges=rbind(edges,random.edges)
+            }else{edges=edges[sample(1:ecount,ecount-n),]}
+            }
+  return(list(edges=edges,ind=indecies))
+}
+
+
+
+get_clusters=function(min.clust,g,nodeList,alpha,replace,add){
+    
+  g=g[g$Source %in% nodeList & g$Target %in% nodeList,]
+  edge.count=dim(g)[1]
+  G=graph.edgelist(cbind(as.character(g$Source),as.character(g$Target)),directed=FALSE)
+  
+  #GET CLUSTERS
+  nDF=data.frame(node=V(G)$name,cluster=as.vector(membership(cluster_louvain(G))),stringsAsFactors=FALSE)
+  cL=clusterList(vector=nDF$cluster,min.clust=min.clust)
+
+  smallNodes=nDF$node[nDF$cluster %in% cL[[2]]]
+  
+  clusters=lapply(cL$cDF$cluster,function(x) nDF$node[nDF$cluster==x])
+  temp_small_edges=sum(g$Source %in% smallNodes | g$Target %in% smallNodes)
+  # NOTE NEED TO ACCOUNT FOR WHEN THERE IS ONLY ONE CLUSTER
+  #update edges
+  
+  # edges from small clusters
+  eobj=getEdges(nodesA=smallNodes,nodesB=smallNodes,g,alpha=alpha,replace=replace,add=add)
+  
+  if(length(clusters)<=1){
+        return(list(edges_out=eobj$edges,clusters='STOP')) 
+  }else{
+        if(replace){g[eobj$ind,]=eobj$edges}else{
+          g=g[!eobj$ind,]
+          g=rbind(g,eobj$edges)
+        }
+        for(i in 1:length(clusters)){
+          eobj=getEdges(nodesA=smallNodes,nodesB=clusters[[i]],g,alpha=alpha,replace=replace,add=add)
+          if(replace){g[eobj$ind,]=eobj$edges}else{
+            g=g[!eobj$ind,]
+            g=rbind(g,eobj$edges)
+          }
+        }
+        
+        
+        # off diagonal edges
+        for(i in length(clusters):2){
+          for(j in (i-1):1){
+            eobj=getEdges(nodesA=clusters[[j]],nodesB=clusters[[i]],g,alpha=alpha,replace=replace,add=add)
+            if(replace){g[eobj$ind,]=eobj$edges}else{
+              g=g[!eobj$ind,]
+              g=rbind(g,eobj$edges)
+            }
+          }
+        }
+ 
+        for(i in 1:length(clusters)){
+          indecies=g$Source %in% clusters[[i]] & g$Target %in% clusters[[i]]
+          g=g[!indecies,]
+        } 
+        
+        return(list(edges_out=g,clusters=clusters))}
+}
+
+# there is a problem within the while loop in the obj$clusters
+
+simIteration=function(nodeList,g,min.clust,alpha,replace,add,membershipM){
+  
+  clustOBJ=get_clusters(min.clust,g,nodeList,alpha,replace,add)
+  
+  queue=clustOBJ$clusters
+  edges=clustOBJ$edges
   
   # should be able to parrelelize this by queu length
   
@@ -72,13 +105,14 @@ simIteration=function(nodeList,g,min.clust,alpha,replace,add){
     nodes=queue[[1]]
     queue[[1]]=NULL
     if(length(nodes)>0){
-      obj=get_clusters(nodeList=nodes,g=m,min.clust,alpha,replace,add)
-      edges=rbind(edges,obj$edgeList)
+      obj=get_clusters(min.clust,g,nodeList=nodes,alpha,replace,add)
+                      
+      edges=rbind(edges,obj$edges)
       
-      if(length(obj$clustList)>0){
+      if(length(obj$clusters)>0){
         n=length(queue)
         
-        for(i in 1:length(obj$clustList)){
+        for(i in 1:length(obj$clusters)){
           if(length(obj$clustList)>0){
             queue[[n+i]]=obj$clustList[[i]]}else{
               queue[[n+i]]=NULL  
@@ -92,7 +126,7 @@ simIteration=function(nodeList,g,min.clust,alpha,replace,add){
   S=graph.empty(n=length(a$userID),directed=FALSE)
   V(S)$name=as.character(a$userID)
   S=add_edges(S,edges=rbind(as.character(edges[,1]),as.character(edges[,2])))
-  
+  tl=testLouvain(S,membership=membershipM)
   
   
   
@@ -102,5 +136,15 @@ simIteration=function(nodeList,g,min.clust,alpha,replace,add){
               trans=transitivity(S),
               dia=diameter(S),
               dyad=count_motifs(S,2),
-              triad=count_motifs(S,3)))}
+              triad=count_motifs(S,3),
+              users=tl$users,
+              groups=tl$groups))}
 
+
+testLouvain=function(G,membership){
+  t=as.data.frame(ftable(paste(membership,as.vector(membership(cluster_louvain(G))),sep='_')))
+  indecies=t$Freq>1
+  users=sum(t$Freq[indecies])
+  groups=sum(indecies)
+  return(list(users=users,groups=groups))
+}
